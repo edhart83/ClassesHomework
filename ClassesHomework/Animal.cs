de2e9dd7bf629df071bb0d7aa5a8594fb78dc03a
jrbeach55 <jrbeach55.2013@gmail.com>
2021-09-06 13:50:29 -0500

    "original"

--- ClassesHomework/Animal.cs
+++ ClassesHomework/Animal.cs
@@ -6,46 +6,5 @@ using System.Threading.Tasks;
 
 namespace ClassesHomework
 {
-    public class Animal
-    {
-        public string Breed {get; protected set;}
-        //protected int Age;        
-        //protected bool IsSitting;
-        //protected string Species;
-        //protected string whatDoesTheFoxSay;
-        
-        public int Age { get; protected set; }
-        public bool IsSitting { get; protected set; }
-        public string Species { get; protected set; }
-        public string whatDoesTheFoxSay { get; protected set; }
-
-        public Animal()
-        {
-            Age = 1;
-            IsSitting = false;
-            Species = "unknown";
-            whatDoesTheFoxSay = "What am I?";
-        }
-        public virtual string Speak()
-        {
-            return whatDoesTheFoxSay;
-        }
-        public virtual string Speak(string sound)
-        {
-            whatDoesTheFoxSay = sound;
-            return whatDoesTheFoxSay;
-        }
-        public void Sit()
-        {
-            IsSitting = true;
-        }
-        public void Stand()
-        {
-            IsSitting = false;
-        }
-        public void CelebrateBirthday()
-        {
-            Age++;
-        }
-    }
+    // write your Animal class here. 
 }
